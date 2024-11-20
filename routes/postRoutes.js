25ab81bba90568cb7ed65234ed842f32255997d8
Ajay Kumar <aks969014@gmail.com>
2024-11-20 16:43:22 +0530

    done updated api sectionn

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 //const postController = require('../controllers/post.controller');
 const upload = require('../middleware/upload');
-const { createBlogPost, getBlogPosts } = require('../controllers/postController');
+const { createBlogPost, getBlogPosts, deleteBlogPost, updateBlogPost } = require('../controllers/postController');
 
 router.post('/createblog',  createBlogPost);
 router.get('/getblog', getBlogPosts)
-// router.get('/', postController.getAllPosts);
+router.delete('/deleteblog/:id', deleteBlogPost)
+router.delete('/updateblog/:id', updateBlogPost)
+//router.get('/deleteblog/:id', deleteBlogPost)
+// router.get('/', postController.getAllPosts);   deleteblog
 // router.get('/:id', postController.getPostById);
 // router.put('/:id', upload.single('image'), postController.updatePost);
 // router.delete('/:id', postController.deletePost);
