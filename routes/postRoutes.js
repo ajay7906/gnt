e47ef167a9f67607259604484f2ffdc4e015477b
Ajay Kumar <aks969014@gmail.com>
2024-11-27 12:19:05 +0530

    done blogpostbyId

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 //const postController = require('../controllers/post.controller');
 const upload = require('../middleware/upload');
-const { createBlogPost, getBlogPosts, deleteBlogPost, updateBlogPost } = require('../controllers/postController');
+const { createBlogPost, getBlogPosts, deleteBlogPost, updateBlogPost, getBlogPostById } = require('../controllers/postController');
 
 router.post('/createblog',  createBlogPost);
-router.get('/getblog', getBlogPosts)
+router.get('/getblog', getBlogPosts);
+router.get('/getblogbyId/:id', getBlogPostById);
 router.delete('/deleteblog/:id', deleteBlogPost)
 router.delete('/updateblog/:id', updateBlogPost)
 //router.get('/deleteblog/:id', deleteBlogPost)
