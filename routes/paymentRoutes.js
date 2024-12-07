ae3c0a9e72b4059e70489df1cdcc127b4d3a7a88
Ajay Kumar <aks969014@gmail.com>
2024-12-07 16:30:28 +0530

    done changes in payment routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createPayment, successPaymentControllers } = require('../controllers/paymentControllers');
+const { createPayment, successPaymentControllers, createPaymentForOrder, successPaymentForOrder } = require('../controllers/paymentControllers');
 const router = express.Router();
 
 
 router.post('/create-payment', createPayment);
 router.post('/status', successPaymentControllers);
+router.post('/paymentOrder', createPaymentForOrder);
+router.post('paymentOrderStatus' , successPaymentForOrder);
 
 module.exports = router;
